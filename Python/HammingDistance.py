e44e0d7c5f91d851baefa94c43e15da8a51e0dbe
Tal <talpogorelis@Gmail.com>
2018-05-13 18:49:35 +0300

    Solution for problem #461
    Solution for problem #461. Hamming Distance

--- Python/HammingDistance.py
+++ Python/HammingDistance.py
@@ -1,3 +1,12 @@
+"""
+The Hamming distance between two integers is the number of positions
+at which the corresponding bits are different.
+
+Given two integers x and y, calculate the Hamming distance.
+
+Note:
+0 ≤ x, y < 231. 
+"""
 class Solution(object):
     def intToBinary(self,x):
         string_x = ""

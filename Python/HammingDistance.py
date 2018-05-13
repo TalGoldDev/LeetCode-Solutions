833f0238a2153c0d3f12512bf98078b90b9f7db6
Tal <talpogorelis@Gmail.com>
2018-05-13 18:47:47 +0300

    Solution for problem #461
    Solution for problem #461. Hamming Distance

---
+++ Python/HammingDistance.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def intToBinary(self,x):
+        string_x = ""
+        n = 31
+        while n > 0:
+            if (x < 2 ** n):
+                string_x += "0"
+            if (x >= 2 ** n):
+                string_x += "1"
+                x -= 2 ** n
+            n -= 1
+        if x == 1:
+            string_x += "1"
+        else:
+            string_x += "0"
+        return string_x
+
+    def hammingDistance(self,x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        count = 0
+        string_x = self.intToBinary(x)
+        string_y = self.intToBinary(y)
+
+        print(string_x)
+        print(string_y)
+
+        for item in range(32):
+            if string_x[item] != string_y[item]:
+                count += 1
+
+        print(count)
+        return count
+
+    
